1f68f1814245280b44714ab2185395f5ba5bca90
wildan <wildan.23az@gmail.com>
2023-03-24 20:49:17 +0700

    modified:   src/handler.js

--- src/handler.js
+++ src/handler.js
@@ -6,31 +6,6 @@ const addBook = (request, h) => {
     name, year, author, summary, publisher, pageCount, readPage, reading,
   } = request.payload;
 
-  const id = nanoid(16);
-  const insertedAt = new Date().toISOString();
-  const updatedAt = insertedAt;
-  const finished = (pageCount === readPage);
-
-  const newBook = {id, name, year, author, summary, publisher, pageCount,
-    readPage, finished, reading, insertedAt, updatedAt,
-  };
-
-  books.push(newBook);
-
-  const isSuccess = books.filter((book) => book.id === id).length > 0;
-
-  if (isSuccess) {
-    const response = h.response({
-      status: 'success',
-      message: 'Buku berhasil ditambahkan',
-      data: {
-        bookId: id,
-      },
-    });
-    response.code(201);
-    return response;
-  }
-
   if (name === undefined) {
     const response = h.response({
       status: 'fail',
@@ -49,6 +24,31 @@ const addBook = (request, h) => {
     return response;
   }
 
+  const id = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = (pageCount === readPage);
+
+  const newBook = {
+    id, name, year, author, summary, publisher, pageCount,
+    readPage, finished, reading, insertedAt, updatedAt,
+  };
+
+  books.push(newBook);
+
+  const isSuccess = books.filter((book) => book.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
   const response = h.response({
     status: 'fail',
     message: 'Buku gagal ditambahkan',
